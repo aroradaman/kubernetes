fc7a0f0d82955c0d6470a64d89e04af5f49fdb67
Daman Arora <aroradaman@gmail.com>
2025-03-18 19:37:09 +0530

    kube-proxy: merge NodePodCIDRHandler with NodeManager
    NodeManager, if configured with to watch for PodCIDR watch, watches
    for changes in PodCIDRs and crashes kube-proxy if a change is
    detected in PodCIDRs.
    
    Signed-off-by: Daman Arora <aroradaman@gmail.com>

--- cmd/kube-proxy/app/server.go
+++ cmd/kube-proxy/app/server.go
@@ -73,6 +73,7 @@ import (
 	"k8s.io/kubernetes/pkg/proxy"
 	"k8s.io/kubernetes/pkg/proxy/apis"
 	kubeproxyconfig "k8s.io/kubernetes/pkg/proxy/apis/config"
+	proxyconfigapi "k8s.io/kubernetes/pkg/proxy/apis/config"
 	proxyconfigscheme "k8s.io/kubernetes/pkg/proxy/apis/config/scheme"
 	"k8s.io/kubernetes/pkg/proxy/config"
 	"k8s.io/kubernetes/pkg/proxy/healthcheck"
@@ -223,8 +224,12 @@ func newProxyServer(ctx context.Context, config *kubeproxyconfig.KubeProxyConfig
 		return nil, fmt.Errorf("can not sync node informer")
 	}
 
-	s.nodeManager = proxy.NewNodeManager(ctx, nodeLister, s.Hostname)
+	s.nodeManager, err = proxy.NewNodeManager(ctx, nodeLister, s.Hostname, s.Config.DetectLocalMode == proxyconfigapi.LocalModeNodeCIDR)
+	if err != nil {
+		return nil, err
+	}
 	rawNodeIPs := s.nodeManager.NodeIPs()
+	s.podCIDRs = s.nodeManager.PodCIDRs()
 	s.PrimaryIPFamily, s.NodeIPs = detectNodeIPs(ctx, rawNodeIPs, config.BindAddress)
 
 	if len(config.NodePortAddresses) == 1 && config.NodePortAddresses[0] == kubeproxyconfig.NodePortAddressesPrimary {
@@ -612,10 +617,6 @@ func (s *ProxyServer) Run(ctx context.Context) error {
 	serviceInformerFactory.Start(wait.NeverStop)
 
 	nodeConfig := config.NewNodeConfig(ctx, s.nodeInformer, s.Config.ConfigSyncPeriod.Duration)
-	// https://issues.k8s.io/111321
-	if s.Config.DetectLocalMode == kubeproxyconfig.LocalModeNodeCIDR {
-		nodeConfig.RegisterEventHandler(proxy.NewNodePodCIDRHandler(ctx, s.podCIDRs))
-	}
 	nodeConfig.RegisterEventHandler(&proxy.NodeEligibleHandler{
 		HealthServer: s.HealthzServer,
 	})
